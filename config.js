af3a552d383d58a63ea977d252b4bd53cf105989
alishagupta20 <alisha.gupta@metadesignsolutions.com.au>
2016-07-11 16:05:35 +0530

    update config.js
    change path

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-'database': 'mongodb://localhost:27017/ouutmaster',
+'database': 'mongodb://mongodb-1,mongodb-2,mongodb-3/ouutmaster',
 'ServerKey':'AIzaSyAr-XetwKudjC99chMSSpjHFCKrwMJ3KQ8'   //'AIzaSyAIV_QCNuPziMYoAWyHRRJDMEDto_r_Z7U' 
 };
